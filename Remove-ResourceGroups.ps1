1873b8562dcf720e98f72894a9ecb6b17a752381
Shad Timm <shad.timm@nwcadence.com>
2016-04-11 11:45:40 -0700

    More formatting updates

--- Remove-ResourceGroups.ps1
+++ Remove-ResourceGroups.ps1
@@ -8,61 +8,59 @@
  
   REQUIRED AUTOMATION ASSETS  
   1. An Automation credential asset called "AzureCredential" that contains the Azure AD user credential with authorization for targeted subscriptions.  
-     To use an asset with a different name you can pass the asset name as a runbook input parameter or change the default value for the input parameter. 
+    To use an asset with a different name you can pass the asset name as a runbook input parameter or change the default value for the input parameter. 
  
 .PARAMETER AzureCredentialAssetName 
-   Optional with default of "AzureCredential". 
-   The name of an Automation credential asset that contains the Azure AD user credential with authorization for this subscription.  
-   To use an asset with a different name you can pass the asset name as a runbook input parameter or change the default value for the input parameter. 
+  Optional with default of "AzureCredential". 
+  The name of an Automation credential asset that contains the Azure AD user credential with authorization for this subscription.  
+  To use an asset with a different name you can pass the asset name as a runbook input parameter or change the default value for the input parameter. 
  
 .PARAMETER ActionType 
-   Mandatory. 
-   The specific action to take for either keeping assets that match the name filter and deleting everything else or deleting just those assets that match the name filter. 
-   Valid values are KEEP, DELETE, and DELETEALL.
+  Mandatory. 
+  The specific action to take for either keeping assets that match the name filter and deleting everything else or deleting just those assets that match the name filter. 
+  Valid values are KEEP, DELETE, and DELETEALL.
     - KEEP = Delete everything except resource groups that match the name filter
-	- DELETE = Delete only resource groups that match the name filter
-	- DELETEALL = Delete all resource groups
+    - DELETE = Delete only resource groups that match the name filter
+    - DELETEALL = Delete all resource groups
  
 .PARAMETER SubscriptionIds 
-   Mandatory 
-   Allows you to specify the targeted subscription id(s) for removal of resource groups.   
-   Pass multiple subscripription ids through a comma separated list.   
+  Mandatory 
+  Allows you to specify the targeted subscription id(s) for removal of resource groups.   
+  Pass multiple subscripription ids through a comma separated list.   
  
 .PARAMETER NameFilter 
-   Optional 
-   Allows you to specify a name filter to limit the resource groups that you will KEEP or DELETE.
-   Pass multiple name filters through a comma separated list.     
-   The filter is not case sensitive and will match any resource group that contains the string.   
+  Optional 
+  Allows you to specify a name filter to limit the resource groups that you will KEEP or DELETE.
+  Pass multiple name filters through a comma separated list.     
+  The filter is not case sensitive and will match any resource group that contains the string.   
  
 .PARAMETER PreviewMode 
-   Optional with default of $true. 
-   Execute the runbook to see which resource groups would be deleted but take no action.  
+  Optional with default of $true. 
+  Execute the runbook to see which resource groups would be deleted but take no action.  
 #> 
 
 workflow Remove-ResourceGroups
 {
-    param(
+	param(
 		[Parameter(Mandatory=$false)]  
 		[string]  $AzureCredentialAssetName = 'AzureCredential', 
 		
-        [parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [ValidateSet('KEEP', 'DELETE', 'DELETEALL')]
-        [string]$ActionType,
+		[parameter(Mandatory = $true)]
+		[string]$ActionType,
 
-        [parameter(Mandatory=$true)]
-        [string]$SubscriptionIds,
+		[parameter(Mandatory=$true)]
+		[string]$SubscriptionIds,
 
-        [parameter(Mandatory = $false)]
-        [string]$NameFilter,
+		[parameter(Mandatory = $false)]
+		[string]$NameFilter,
 		
-        [parameter(Mandatory = $false)]
-        [bool]$PreviewMode = $true
-    )
+		[parameter(Mandatory = $false)]
+		[bool]$PreviewMode = $true
+	)
 
 
-    # Returns strings with status messages 
-    [OutputType([String])]
+	# Returns strings with status messages 
+	[OutputType([String])]
 	
 	$VerbosePreference = 'Continue'
 	
@@ -71,23 +69,23 @@ workflow Remove-ResourceGroups
 		throw "ActionType not valid, valid actions are KEEP, DELETE, and DELETEALL"
 	}
  
-    # Connect to Azure and select the subscription to work against
-    $creds = Get-AutomationPSCredential -Name $AzureCredentialAssetName
+	# Connect to Azure and select the subscription to work against
+	$creds = Get-AutomationPSCredential -Name $AzureCredentialAssetName
  
-    $null = Add-AzureRmAccount -Credential $creds -ErrorVariable err 
-    if($err) { 
+	$null = Add-AzureRmAccount -Credential $creds -ErrorAction Stop -ErrorVariable err 
+	if($err) { 
 		throw "Failed to log in to Azure RM. $err"
-    } 
+	} 
  
 	# Parse subscription id list and name filter list
-    $subscriptionIdList = $SubscriptionIds.Split(',')
+	$subscriptionIdList = $SubscriptionIds.Split(',')
 	if ($NameFilter) {
 		$nameFilterList = $NameFilter.Split(',')
 		[regex]$nameFilterRegex = ‘(‘ + (($nameFilterList | foreach {[regex]::escape($_.ToLower())}) –join “|”) + ‘)’
 	}
 
 	# Begin loop through each subscription
-    foreach ($subscriptionId in $subscriptionIdList) {
+	foreach ($subscriptionId in $subscriptionIdList) {
 		try {
 			# Select the subscription, if not found, skip resource group removal
 			Write-Verbose "Attempting connection to subscription: $subscriptionId"
@@ -101,11 +99,11 @@ workflow Remove-ResourceGroups
 				# Find resource groups to remove based on passed in name filter and KEEP, DELETE, or DELEETALL action
 				if ($ActionType.ToUpper() -eq 'KEEP') {
 					$groupsToRemove = Get-AzureRmResourceGroup | `
-									? { $nameFilterList.Count -eq 0 -or $_.ResourceGroupName.ToLower() -notmatch $nameFilterRegex }
+										? { $nameFilterList.Count -eq 0 -or $_.ResourceGroupName.ToLower() -notmatch $nameFilterRegex }
 				}
 				elseif ($ActionType.ToUpper() -eq 'DELETE') {
 					$groupsToRemove = Get-AzureRmResourceGroup | `
-									? { $nameFilterList.Count -eq 0 -or $_.ResourceGroupName.ToLower() -match $nameFilterRegex }
+										? { $nameFilterList.Count -eq 0 -or $_.ResourceGroupName.ToLower() -match $nameFilterRegex }
 				}
 				elseif ($ActionType.ToUpper() -eq 'DELETEALL') {
 					$groupsToRemove = Get-AzureRmResourceGroup
@@ -154,5 +152,5 @@ workflow Remove-ResourceGroups
 		if ($errorMessage) {
 			Write-Error $errorMessage
 		}
-    }
+	}
 }
